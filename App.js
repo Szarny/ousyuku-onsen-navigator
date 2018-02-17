61f3834668ff7756cb2652d8732d0cdf6aaa20f9
Tsubasa <tsubasa.umeuchi@gmail.com>
2018-02-17 17:52:38 +0900

    [update] 画面遷移機能・リストビュー機能・マップ表示機能の実装

--- App.js
+++ App.js
@@ -2,9 +2,9 @@
  * モジュールのインポート
  */
 import React, { Component } from 'react';
-import { Alert } from 'react-native';
-import { Container, Header, Title, Content, List, ListItem, Left, Body, Right, Thumbnail, Text } from 'native-base'; // 2.3.8
-import { Constants } from 'expo';
+import { Image } from 'react-native';
+import { Container, Content, List, ListItem, Left, Body, Right, Thumbnail, Text } from 'native-base'; // 2.3.8
+import { Constants, MapView, Marker } from 'expo';
 import { StackNavigator } from 'react-navigation';
 
 /**
@@ -21,7 +21,7 @@ export class HomeScreen extends Component {
 
   render() {
     /**
-     * homeListItemsを
+     * homeListItemsをmapによって，エレメント群に変換する
      */
     const listItemsElements = homeListItems.map((listItem, index) => {
       return (
@@ -53,144 +53,47 @@ export class HomeScreen extends Component {
   }
 }
 
+/**
+ * 日帰り温泉入浴施設画面
+ */
 export class HotSpaScreen extends Component {
   static navigationOptions = {
     title: 'HotSpa'
   };
 
   render() {
+    const listItemsElements = hotSpaListItems.map((listItem, index) => {
+      return (
+        <ListItem
+          avatar
+          key={index.toString()}
+          onPress={() => {
+            this.props.navigation.navigate('Map', {
+              data: listItem
+            });
+          }}
+        >
+          <Left>
+            <Thumbnail source={{ uri: listItem.thumbnailSourceUri }} />
+          </Left>
+          <Body>
+            <Text>{listItem.bodyText}</Text>
+            <Text note>{listItem.tel}</Text>
+            <Text note>{listItem.time}</Text>
+            <Text note>{listItem.price}</Text>
+          </Body>
+          <Right>
+            <Text>{listItem.roten}</Text>
+          </Right>
+        </ListItem>
+      )
+    });
+
     return (
-      <Container>
+      <Container style={styles.containerStyle}>
         <Content>
           <List>
-            <ListItem avatar>
-              <Left>
-                <Thumbnail source={{ uri: 'https://goo.gl/cfnrV5' }} />
-              </Left>
-              <Body>
-                <Text>施設名称</Text>
-                <Text note>(Tel) 営業時間 [料金]</Text>
-              </Body>
-              <Right>
-                <Text>(露天)</Text>
-              </Right>
-            </ListItem>
-            <ListItem avatar>
-              <Left>
-                <Thumbnail source={{ uri: 'https://goo.gl/cfnrV5' }} />
-              </Left>
-              <Body>
-                <Text>ホテル森の風鴬宿</Text>
-                <Text note>019-695-3333</Text>
-                <Text note>11:00～18:00</Text>
-                <Text note>入浴料 1050 (小)525</Text>
-              </Body>
-              <Right>
-                <Text>(有り)</Text>
-              </Right>
-            </ListItem>
-
-            <ListItem avatar>
-              <Left>
-                <Thumbnail source={{ uri: 'https://goo.gl/cfnrV5' }} />
-              </Left>
-              <Body>
-                <Text>長栄館</Text>
-                <Text note>019-695-2121</Text>
-                <Text note>11:00～15:00</Text>
-                <Text note>入浴料 ￥1000 /(小)500</Text>
-              </Body>
-              <Right>
-                <Text>(有り)</Text>
-              </Right>
-            </ListItem>
-
-            <ListItem avatar>
-              <Left>
-                <Thumbnail source={{ uri: 'https://goo.gl/cfnrV5' }} />
-              </Left>
-              <Body>
-                <Text>ホテル加賀助</Text>
-                <Text note>019-695-2216</Text>
-                <Text note>13:00～19:00</Text>
-                <Text note>入浴料 500 /(小)250</Text>
-              </Body>
-              <Right>
-                <Text>(無し)</Text>
-              </Right>
-            </ListItem>
-
-            <ListItem avatar>
-              <Left>
-                <Thumbnail source={{ uri: 'https://goo.gl/cfnrV5' }} />
-              </Left>
-              <Body>
-                <Text />
-                <Text note />
-                <Text note>入浴料  /(小) </Text>
-              </Body>
-              <Right>
-                <Text>(有り)</Text>
-              </Right>
-            </ListItem>
-
-            <ListItem avatar>
-              <Left>
-                <Thumbnail source={{ uri: 'https://goo.gl/cfnrV5' }} />
-              </Left>
-              <Body>
-                <Text>ホテル森の風鴬宿</Text>
-                <Text note>019-695-3333</Text>
-                <Text note>11:00～15:00</Text>
-                <Text note>入浴料 1000 /(小)500</Text>
-              </Body>
-              <Right>
-                <Text>(有り)</Text>
-              </Right>
-            </ListItem>
-
-            <ListItem avatar>
-              <Left>
-                <Thumbnail source={{ uri: 'https://goo.gl/cfnrV5' }} />
-              </Left>
-              <Body>
-                <Text>長栄館   (019-695-3333)</Text>
-                <Text note>11:00～15:00</Text>
-                <Text note>入浴料 1000 /(小)500</Text>
-              </Body>
-              <Right>
-                <Text>(有り)</Text>
-              </Right>
-            </ListItem>
-
-            <ListItem avatar>
-              <Left>
-                <Thumbnail source={{ uri: 'https://goo.gl/cfnrV5' }} />
-              </Left>
-              <Body>
-                <Text>ホテル森の風鴬宿   (019-695-3333)</Text>
-                <Text note>11:00～15:00</Text>
-                <Text note>入浴料 1000 /(小)500</Text>
-              </Body>
-              <Right>
-                <Text>(有り)</Text>
-              </Right>
-            </ListItem>
-
-            <ListItem avatar>
-              <Left>
-                <Thumbnail source={{ uri: 'https://goo.gl/cfnrV5' }} />
-              </Left>
-              <Body>
-                <Text>ホテル森の風鴬宿   (019-695-3333)</Text>
-                <Text note>11:00～15:00</Text>
-                <Text note>入浴料 1000 /(小)500</Text>
-              </Body>
-              <Right>
-                <Text>(有り)</Text>
-              </Right>
-            </ListItem>
-
+            {listItemsElements}
           </List>
         </Content>
       </Container>
@@ -198,6 +101,82 @@ export class HotSpaScreen extends Component {
   }
 }
 
+/**
+ * マップ画面
+ */
+export class MapScreen extends Component {
+  static navigationOptions = {
+    title: 'Map'
+  };
+
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    /**
+     * Navigationからの値の受け渡し
+     */
+    const { params } = this.props.navigation.state;
+    const bodyText   = params ? params.data.bodyText : null;
+    const lat        = params ? params.data.lat : null;
+    const lng        = params ? params.data.lng : null;
+    const src        = params ? params.data.thumbnailSourceUri : null;
+    const tel        = params ? params.data.tel : null;
+    const time       = params ? params.data.time : null;
+    const price      = params ? params.data.price : null;
+    const roten      = params ? params.data.roten : null;
+
+    // 緯度経度情報
+    const mapRegion   = {
+      latitude: lat,
+      longitude: lng,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01
+    }
+
+    return (
+      <Container style={styles.containerStyle}>
+        <Content>
+          <MapView
+            style={{ alignSelf: 'stretch', height: 350 }}
+            region={mapRegion}
+            zoomEnabled={true}
+            rotateEnabled={true}
+            scrollEnabled={true}
+            >
+            <MapView.Marker
+              coordinate={mapRegion}
+              title={bodyText}
+              description={tel}
+            />
+          </MapView>
+
+          <List>
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={{ uri: src }} />
+              </Left>
+              <Body>
+                <Text>{bodyText}</Text>
+                <Text note>{tel}</Text>
+                <Text note>{time}</Text>
+                <Text note>{price}</Text>
+              </Body>
+              <Right>
+                <Text>{roten}</Text>
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+/**
+ * Navigation(画面遷移)の設定
+ */
 export default StackNavigator(
   {
     Home: {
@@ -206,13 +185,18 @@ export default StackNavigator(
     HotSpa: {
       screen: HotSpaScreen,
     },
+    Map: {
+      screen: MapScreen,
+    },
   },
   {
     initialRouteName: 'Home',
   }
 );
 
-
+/**
+ * ホーム画面に表示するリストのデータ
+ */
 const homeListItems = [
   {
     thumbnailSourceUri: "https://goo.gl/cfnrV5",
@@ -241,8 +225,42 @@ const homeListItems = [
   },
 ];
 
+/**
+ * 日帰り温泉画面に表示するリストのデータ
+ */
+const hotSpaListItems = [
+  {
+    thumbnailSourceUri: "https://goo.gl/cfnrV5",
+    bodyText: "ホテル森の風鴬宿",
+    tel: "019-695-3333",
+    time: "11:00～18:00",
+    price: "入浴料 1050 (小)525",
+    roten: "(有り)",
+    lat: 39.638157,
+    lng: 140.933680
+  },
+  {
+    thumbnailSourceUri: "https://goo.gl/cfnrV5",
+    bodyText: "ホテル加賀助",
+    tel: "019-695-2216",
+    time: "13:00～18:00",
+    price: "入浴料 500 (小)250",
+    roten: "(無し)",
+    lat: 39.636090,
+    lng: 140.921975
+  }
+];
+
+/**
+ * スタイルの定義
+ */
 const styles = {
   containerStyle: {
     paddingTop: Constants.statusBarHeight
   },
+  mapStyle: {
+    flex: 1,
+    justifyContent: "space-around",
+    alignItems: "center",
+  }
 }
